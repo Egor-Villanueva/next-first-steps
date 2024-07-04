735516362501a011cbcd146f3ab8212e3e7451f9
Egor Villanueva <egorvillanuevaespinoza@gmail.com>
2024-07-04 17:45:31 -0500

    First commit

---
+++ app/(general)/about/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+    title: 'SEO Title',
+    description: 'SEO Description'
+}
+
+export default function AboutPage() {
+    return (
+        <div>About Page</div>
+    )
+}
